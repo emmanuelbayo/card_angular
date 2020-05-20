9dabdd304f7c1f0b533c42be1b4fafde572847e4
emmanuelbayo <bayopatrick2@gmail.com>
2020-05-20 11:44:35 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'card-project';
+  posts = [
+    {
+      title : 'Neat Tree',
+      imageURL : 'assets/tree.jpeg',
+      username : 'nature',
+      content : 'I saw this neat tree today',
+    },
+    {
+      title : 'Snowy Mountain',
+      imageURL : 'assets/mountain.jpeg',
+      username : 'mountainlover',
+      content : 'Here is a picture of a snowy mountain',
+    },
+    {
+      title : 'Mountain Biking',
+      imageURL : 'assets/biking.jpeg',
+      username : 'biking12222',
+      content : 'I did some biking today',
+    },
+    {
+      title : 'Mountain Biking',
+      imageURL : 'assets/biking.jpeg',
+      username : 'biking12222',
+      content : 'I did some biking today',
+    }
+  ];
+
 }
